86456b39dac262e092a1fd36a5562c03842bf322
Seung Hoon Lee <slee3471@usc.edu>
2024-01-26 22:12:30 +0000

    part 1

--- split.cpp
+++ split.cpp
@@ -18,6 +18,28 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+  if (in == nullptr) { // basecase
+    return;
+  }
+
+  Node* nextNode = in->next;
+
+  if (in->value % 2 == 1) { // if the value that is being pointed at is odd
+    in->next = odds; // sets pointer to point at odds
+    odds = in; // updates head of odd to be the current node
+  }
+
+  else {
+    in->next = evens; // sets pointer to point at evens
+    evens = in; // updates head of even to be the current node
+  }
+
+  split(nextNode, odds, evens); // when using nextNode to update, keeps failing input set to nulldfiojasdf;jkfsadj;k
+
+  if (nextNode == nullptr) { // modify original list pointer
+    in = nullptr;
+  }
+
 }
 
 /* If you needed a helper function, write it here */
